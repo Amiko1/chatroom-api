0b986a5931b443aa04c28bb1715121f5bf0b1f1f
amiko <amiraniburjanadze@gmail.com>
2022-05-14 13:56:31 +0400

    added logout and logout all

--- src/routers/users.js
+++ src/routers/users.js
@@ -7,27 +7,51 @@ const UserModel = require('../models/user')
 router.post('/register', async (req, res) => {
 
     const userDoc = new UserModel(req.body);
-
+    
     try {
         await userDoc.save()
         const token = await userDoc.generateAuthToken()
         res.status(200).send({userDoc, token})
     } catch (e) {
-        console.log(e)
         res.status(400).send(e)
     }
 })
 
 router.post('/login', async (req, res) => {
-
+    
     try {
         const userDoc = await UserModel.findByCredentials(req.body.email, req.body.password)
         const token = await userDoc.generateAuthToken()
-        res.status(200).send({user, token})
+        res.status(200).send({userDoc, token})
     } catch (e) {
         res.status(500).send(e)
     }
 })
+
+router.post('/logout', auth, async (req, res) => {
+    try {
+        req.user.tokens = req.user.tokens.filter(token => {
+            return token.token !== req.token;
+        })
+        await req.user.save()
+
+        res.send()
+    } catch (e) {
+        res.status(500).send()
+    }
+})
+
+router.post('/logoutall', auth, async (req,res) => {
+    try {
+        req.user.tokens = []
+        req.user.save()
+        res.send()
+    } catch (e) {
+        res.status(500).send()
+    }
+})
+
+
 
 router.get('/me', auth, async (req,res) => {
    res.send(req.user)

6f08386bd735ac3e45b488b4775ed410039abe14
amiko <amiraniburjanadze@gmail.com>
2022-05-14 13:35:40 +0400

    added auth middleware

--- src/routers/users.js
+++ src/routers/users.js
@@ -9,42 +9,28 @@ router.post('/register', async (req, res) => {
     const userDoc = new UserModel(req.body);
 
     try {
-
         await userDoc.save()
         const token = await userDoc.generateAuthToken()
-
         res.status(200).send({userDoc, token})
-
     } catch (e) {
         console.log(e)
         res.status(400).send(e)
-
     }
 })
 
 router.post('/login', async (req, res) => {
 
     try {
-
         const userDoc = await UserModel.findByCredentials(req.body.email, req.body.password)
         const token = await userDoc.generateAuthToken()
-
         res.status(200).send({user, token})
-
     } catch (e) {
-    
         res.status(500).send(e)
-
     }
 })
 
-router.get('/me', async (req,res) => {
-    try {
-        const user = await UserModel.find({})
-        res.send(user)
-    } catch (e) {
-        res.status(500).send(e)
-    }
+router.get('/me', auth, async (req,res) => {
+   res.send(req.user)
 })
 
 
